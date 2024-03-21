73c377bea5cf211d352b90fe5bcac4263f54eac5
eduardopassos55551@gmail.com <eduardopassos55551@gmail.com>
2024-03-21 15:04:22 -0300

    Tela de Formulário usando Kotlin

---
+++ app/src/main/java/edublt/com/form_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package edublt.com.form_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
